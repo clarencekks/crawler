a1474bf8f60559208d7185fc52b31c67f48bee48
linsamtw <samlin266118@gmail.com>
2026-05-07 21:03:31 +0800

    feat: update mydb

--- crawler/upload_duplicate_data_to_mysql.py
+++ crawler/upload_duplicate_data_to_mysql.py
@@ -18,7 +18,7 @@ from crawler.config import MYSQL_ACCOUNT, MYSQL_HOST, MYSQL_PASSWORD, MYSQL_PORT
 
 # 建立連接到 MySQL 的資料庫引擎（記得把帳號密碼換成你自己的）
 engine = create_engine(
-    f"mysql+pymysql://{MYSQL_ACCOUNT}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/tibame"
+    f"mysql+pymysql://{MYSQL_ACCOUNT}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/mydb"
 )
 
 # 定義資料表結構，對應到 MySQL 中的 test_duplicate 表
